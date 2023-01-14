65b920eeae22c796137a4589d48058d3db300c0e
Wen Junhua <wenjh1998@gmail.com>
2023-01-14 18:08:54 +0800

    feat: add admin navbar

--- src/pages/admin/AdminPage.js
+++ src/pages/admin/AdminPage.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Routes, useLocation, Route } from "react-router-dom";
+import { AdminMenuLink, AdminNav } from "./AdminPageElements";
 
 function AdminPage() {
-  return <div>AdminPage</div>;
+  const location = useLocation();
+  return (
+    <div>
+      <AdminNav>
+        <AdminMenuLink to="">Store Info</AdminMenuLink>
+        <AdminMenuLink to="dishes">Dishes</AdminMenuLink>
+        <AdminMenuLink to="staff">Staff</AdminMenuLink>
+        <AdminMenuLink to="orders">Orders</AdminMenuLink>
+      </AdminNav>
+      <Routes>
+        <Route path="/" element={"Store Info"} />
+        <Route path="/dishes" element={"Dishes"} />
+        <Route path="/staff" element={"Staff"} />
+        <Route path="/orders" element={"Orders"} />
+      </Routes>
+    </div>
+  );
 }
 
 export default AdminPage;
--- src/pages/admin/AdminPageElements.js
+++ src/pages/admin/AdminPageElements.js
@@ -1,2 +1,13 @@
+import React from "react";
 import styled from "styled-components";
-import { Colors } from "./Theme";
+import { Colors } from "../../Theme";
+import { MenuLink } from "../../components/NavBar/NavBarElements";
+import NavBar from "../../components/NavBar/NavBar";
+
+export const AdminMenuLink = styled(MenuLink)`
+  color: ${Colors.black};
+  display: inline-block;
+  padding: 1rem;
+`;
+
+export const AdminNav = styled(NavBar)``;
