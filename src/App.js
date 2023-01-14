a2506c0942ddf1a4650c59c444e5ca7cd3ffeea6
Rui En <neoruien@u.nus.edu>
2023-01-14 20:48:54 +0800

    Add orderpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CashierPage from "./pages/cashier/CashierPage";
+import OrderPage from "./pages/cashier/OrderPage";
 import KitchenPage from "./pages/kitchen/KitchenPage";
 import AdminPage from "./pages/admin/AdminPage";
 import BuyerPage from "./pages/buyer/BuyerPage";
@@ -10,6 +11,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path="/cashier" element={<CashierPage />} />
+        <Route exact path="/cashier/order" element={<OrderPage />} />
+
         <Route exact path="/kitchen" element={<KitchenPage />} />
         <Route exact path="/admin" element={<AdminPage />} />
         <Route exact path="/buyer" element={<BuyerPage />} />
---
+++ src/pages/cashier/OrderPageElements.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { Colors } from "../../Theme";
+
+export const Main = styled.div`
+  background-color: ${Colors.white};
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+`;
