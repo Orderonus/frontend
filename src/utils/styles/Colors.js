04ac0e84d625ccf8486f872188b2d553a7b66b29
Rui En <neoruien@u.nus.edu>
2023-01-14 13:59:57 +0800

    Add colours

---
+++ src/utils/styles/Colors.js
@@ -0,0 +1,11 @@
+export const Colors = {
+  primary: "#FF7B54",
+  secondary: "#FFB26B",
+  tertiary: "#FFD56F",
+  black: "#313131",
+  offwhite: "#FFFFF0",
+  red: "#C64545",
+  green: "#939B62",
+  grey: "#989890",
+  lightgrey: "#E4E4E4",
+};
